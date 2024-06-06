52eab910d17fc33d27c024e0eaade5b7d190647d
thasleemkhan762 <155559104+thasleemkhan762@users.noreply.github.com>
2024-06-06 16:01:50 +0530

    revert

--- src/components/layouts/MainLayout/MainLayout.js
+++ src/components/layouts/MainLayout/MainLayout.js
@@ -14,7 +14,7 @@ const MainLayout = () => {
 
   useEffect(() => {
     axios
-      .get("https://react-backend-blog.vercel.app/")
+      .get("http://localhost:5000/api/blogs")
       .then((response) => {
         console.log("data:", response.data);
         const blogs = response.data.slice().reverse();
